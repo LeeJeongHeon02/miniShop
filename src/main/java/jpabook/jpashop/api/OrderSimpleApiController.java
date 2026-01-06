3404b1ce8017263270673be95ec1fd911149b84d
이정헌 <aoc05230@naver.com>
2026-01-07 00:19:03 +0900

    change database datasource url

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -6,13 +6,14 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -28,6 +29,7 @@ import static java.util.stream.Collectors.toList;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     /*
     V1. 엔티티 직접 노출
@@ -78,6 +80,19 @@ public class OrderSimpleApiController {
     }
 
 
+    /**
+     * V4. JPA에서 DTO로 바로 조회
+     * - 쿼리 1번 호출
+     * - select 절에서 원하는 데이터만 선택해서 조회
+     */
+    @GetMapping(value = "/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
+
+
     @Data
     static class SimpleOrderDto {
         private Long orderId;
@@ -94,4 +109,5 @@ public class OrderSimpleApiController {
             this.address = order.getDelivery().getAddress(); //Lazy 강제 초기화
         }
     }
+
 }
